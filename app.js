29b13e9d082e76c3e72fbd06e7c3b851cd7be290
AdarshThakur398 <nutius90@gmail.com>
2024-05-25 19:26:11 +0530

    added json

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const userRoutes = require('./routes/users')
 const helmet=require('helmet')
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp' //process.env.DB_URL
+const dbUrl = process.env.DB_URL
 if (!campgroundSchema || !reviewSchema) {
     console.error("Schema objects are not properly initialized or imported.");
  
